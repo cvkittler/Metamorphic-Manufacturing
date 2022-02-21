7d22d649638aa2283461c963079aad659d03af1a
Charles Kittler <cvkittler@gmail.com>
2022-02-21 12:37:00 -0500

    refactoring code so more functionality can be added

--- mmm_mqp_base/scripts/mmm_joint_sender.py
+++ mmm_mqp_base/scripts/mmm_joint_sender.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-
-from copy import deepcopy
-from os import kill
-
-from click import command
 import rospy
 
 import Tkinter as tk
@@ -29,180 +24,185 @@ toggleButtonState = True
 eoatPub = None
 
 def main():
-    master = tk.Tk()
-    master.title("MMM Simple Sender")
-    master.protocol("WM_DELETE_WINDOW", lambda: killPorgram(master))
+    mainTk = tk.Tk()
+    mainTk.title("MMM Simple Sender")
+    mainTk.protocol("WM_DELETE_WINDOW", lambda: killPorgram(mainTk))
 
-    valid = master.register(validateJointInput)
+    valid = mainTk.register(validateJointInput)
 
-    frameLeft = tk.Frame(master)
-    frameLeft.grid(column=0,row=0, sticky="nsew")
+    jointSpacePoseFrame = tk.Frame(mainTk)
+    jointSpacePoseFrame.grid(column=0,row=0, sticky="nsew")
 
-    framecenter = tk.Frame(master)
-    framecenter.grid(column=1,row=0, sticky="nsew")
+    curJointAnglesFrame = tk.Frame(mainTk)
+    curJointAnglesFrame.grid(column=1,row=0, sticky="nsew")
 
-    frameleftAlt = tk.Frame(master)
-    frameleftAlt.grid(column=0,row=0, sticky="nsew")
-    frameLeft.tkraise()
+    workSpacePoseFrame = tk.Frame(mainTk)
+    workSpacePoseFrame.grid(column=0,row=0, sticky="nsew")
+    jointSpacePoseFrame.tkraise()
 
-    frameRight = tk.Frame(master)
-    frameRight.grid(column=2,row=0, sticky="nsew")
+    curWorkspacePoseFrame = tk.Frame(mainTk)
+    curWorkspacePoseFrame.grid(column=2,row=0, sticky="nsew")
 
-    eoatFrame = tk.Frame(master)
+    eoatFrame = tk.Frame(mainTk)
     eoatFrame.grid(column=3,row=0, sticky="nsew")
 
     moveit_commander.roscpp_initialize(sys.argv)
     group = moveit_commander.MoveGroupCommander(group_name)
 
+    # pose Controller 
+
+    button1=tk.Button(mainTk, text="button1")
+    button1.grid(column=1,row=1)
+
     #frame left
-    titleLabelLeft = tk.Label(frameLeft,text="Send Joint Targets")
+    titleLabelLeft = tk.Label(jointSpacePoseFrame,text="Send Joint Targets")
     titleLabelLeft.pack(pady = (10,20),padx = (10,10))
     titleLabelLeft.config(font=("Courier", 15))
 
-    l1 = tk.Label(frameLeft,text="Joint 1")
-    l2 = tk.Label(frameLeft,text="Joint 2")
-    l3 = tk.Label(frameLeft,text="Joint 3")
-    l4 = tk.Label(frameLeft,text="Joint 4")
-    l5 = tk.Label(frameLeft,text="Joint 5")
-    l6 = tk.Label(frameLeft,text="Joint 6")
+    l1 = tk.Label(jointSpacePoseFrame,text="Joint 1")
+    l2 = tk.Label(jointSpacePoseFrame,text="Joint 2")
+    l3 = tk.Label(jointSpacePoseFrame,text="Joint 3")
+    l4 = tk.Label(jointSpacePoseFrame,text="Joint 4")
+    l5 = tk.Label(jointSpacePoseFrame,text="Joint 5")
+    l6 = tk.Label(jointSpacePoseFrame,text="Joint 6")
 
-    e1 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 1))
+    e1 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 1))
     l1.pack(pady=10)
     e1.pack()
     e1.insert(-1,"0")
-    e2 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 2))
+    e2 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 2))
     l2.pack(pady=10)
     e2.pack()
     e2.insert(-1,"0")
-    e3 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 3))
+    e3 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 3))
     l3.pack(pady=10)
     e3.pack()
     e3.insert(-1,"0")
-    e4 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 4))
+    e4 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 4))
     l4.pack(pady=10)
     e4.pack()
     e4.insert(-1,"0")
-    e5 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 5))
+    e5 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 5))
     l5.pack(pady=10)
     e5.pack()
     e5.insert(-1,"0")
-    e6 = tk.Entry(frameLeft, validate="key", validatecommand=(valid, '%P', 6))
+    e6 = tk.Entry(jointSpacePoseFrame, validate="key", validatecommand=(valid, '%P', 6))
     l6.pack(pady=10)
     e6.pack()
     e6.insert(-1,"0")
 
-    jButton = tk.Button(frameLeft, 
+    jButton = tk.Button(jointSpacePoseFrame, 
                         text="Send Target Angel", 
                         command= lambda: jointAngleButtonEnvoke(group,e1.get(),e2.get(),e3.get(),e4.get(),e5.get(),e6.get())
                         )
     jButton.pack(pady=20)
 
     #alt left frame start 
-    titleLabelLeftalt = tk.Label(frameleftAlt,text="Send Work Space Targets")
+    titleLabelLeftalt = tk.Label(workSpacePoseFrame,text="Send Work Space Targets")
     titleLabelLeftalt.pack(pady = (10,20),padx = (10,10))
     titleLabelLeftalt.config(font=("Courier", 15))
-    l1alt = tk.Label(frameleftAlt,text="X")
-    l2alt = tk.Label(frameleftAlt,text="Y")
-    l3alt = tk.Label(frameleftAlt,text="Z")
-    l4alt = tk.Label(frameleftAlt,text="Rotation X")
-    l5alt = tk.Label(frameleftAlt,text="Rotation Y")
-    l6alt = tk.Label(frameleftAlt,text="Rotation Z")
-    e1alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "X"))
+    l1alt = tk.Label(workSpacePoseFrame,text="X")
+    l2alt = tk.Label(workSpacePoseFrame,text="Y")
+    l3alt = tk.Label(workSpacePoseFrame,text="Z")
+    l4alt = tk.Label(workSpacePoseFrame,text="Rotation X")
+    l5alt = tk.Label(workSpacePoseFrame,text="Rotation Y")
+    l6alt = tk.Label(workSpacePoseFrame,text="Rotation Z")
+    e1alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "X"))
     l1alt.pack(pady=10)
     e1alt.pack()
     e1alt.insert(-1,"0.815")
-    e2alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "Y"))
+    e2alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "Y"))
     l2alt.pack(pady=10)
     e2alt.pack()
     e2alt.insert(-1,"0")
-    e3alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "Z"))
+    e3alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "Z"))
     l3alt.pack(pady=10)
     e3alt.pack()
     e3alt.insert(-1,"1.185")
-    e4alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "rX"))
+    e4alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "rX"))
     l4alt.pack(pady=10)
     e4alt.pack()
     e4alt.insert(-1,"0")
-    e5alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "rY"))
+    e5alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "rY"))
     l5alt.pack(pady=10)
     e5alt.pack()
     e5alt.insert(-1,"90")
-    e6alt = tk.Entry(frameleftAlt, validate="key", validatecommand=(valid, '%P', "rZ"))
+    e6alt = tk.Entry(workSpacePoseFrame, validate="key", validatecommand=(valid, '%P', "rZ"))
     l6alt.pack(pady=10)
     e6alt.pack()
     e6alt.insert(-1,"0")
 
-    jButton = tk.Button(frameleftAlt, 
+    jButton = tk.Button(workSpacePoseFrame, 
                         text="Send Target Position", 
                         command= lambda: targetPoseButtonEnvoke(group,e1alt.get(),e2alt.get(),e3alt.get(),e4alt.get(),e5alt.get(),e6alt.get())
                         )
     jButton.pack(pady=20)
 
     #frame center start
-    titleLabelcenter = tk.Label(framecenter,text="Current Joint Angles")
+    titleLabelcenter = tk.Label(curJointAnglesFrame,text="Current Joint Angles")
     titleLabelcenter.pack(pady = (10,20),padx = (10,10))
     titleLabelcenter.config(font=("Courier", 15))
 
-    l1center = tk.Label(framecenter,text="Joint 1")
-    l2center = tk.Label(framecenter,text="Joint 2")
-    l3center = tk.Label(framecenter,text="Joint 3")
-    l4center = tk.Label(framecenter,text="Joint 4")
-    l5center = tk.Label(framecenter,text="Joint 5")
-    l6center = tk.Label(framecenter,text="Joint 6")
+    l1center = tk.Label(curJointAnglesFrame,text="Joint 1")
+    l2center = tk.Label(curJointAnglesFrame,text="Joint 2")
+    l3center = tk.Label(curJointAnglesFrame,text="Joint 3")
+    l4center = tk.Label(curJointAnglesFrame,text="Joint 4")
+    l5center = tk.Label(curJointAnglesFrame,text="Joint 5")
+    l6center = tk.Label(curJointAnglesFrame,text="Joint 6")
 
-    e1center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e1center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l1center.pack(pady=10)
     e1center.pack()
-    e2center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e2center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l2center.pack(pady=10)
     e2center.pack()
-    e3center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e3center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l3center.pack(pady=10)
     e3center.pack()
-    e4center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e4center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l4center.pack(pady=10)
     e4center.pack()
-    e5center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e5center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l5center.pack(pady=10)
     e5center.pack()
-    e6center = tk.Label(framecenter, borderwidth=1, relief="solid", width=25)
+    e6center = tk.Label(curJointAnglesFrame, borderwidth=1, relief="solid", width=25)
     l6center.pack(pady=10)
     e6center.pack()
     
-    jButtoncenter = tk.Button(framecenter, 
+    jButtoncenter = tk.Button(curJointAnglesFrame, 
                         text="Switch Goal to Work Space", 
-                        command= lambda: frameToggle(frameleftAlt,frameLeft, jButtoncenter)
+                        command= lambda: frameToggle(workSpacePoseFrame,jointSpacePoseFrame, jButtoncenter)
                         )
     jButtoncenter.pack(pady=20)
 
     #frame right start
-    titleLabelright = tk.Label(frameRight,text="Current End Effector Pose")
+    titleLabelright = tk.Label(curWorkspacePoseFrame,text="Current End Effector Pose")
     titleLabelright.pack(pady = (10,20),padx = (10,10))
     titleLabelright.config(font=("Courier", 15))
 
-    l1right = tk.Label(frameRight,text="X")
-    l2right = tk.Label(frameRight,text="Y")
-    l3right = tk.Label(frameRight,text="Z")
-    l4right = tk.Label(frameRight,text="Rotation X")
-    l5right = tk.Label(frameRight,text="Rotation Y")
-    l6right = tk.Label(frameRight,text="Rotation Z")
+    l1right = tk.Label(curWorkspacePoseFrame,text="X")
+    l2right = tk.Label(curWorkspacePoseFrame,text="Y")
+    l3right = tk.Label(curWorkspacePoseFrame,text="Z")
+    l4right = tk.Label(curWorkspacePoseFrame,text="Rotation X")
+    l5right = tk.Label(curWorkspacePoseFrame,text="Rotation Y")
+    l6right = tk.Label(curWorkspacePoseFrame,text="Rotation Z")
 
-    e1right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e1right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l1right.pack(pady=10)
     e1right.pack()
-    e2right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e2right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l2right.pack(pady=10)
     e2right.pack()
-    e3right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e3right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l3right.pack(pady=10)
     e3right.pack()
-    e4right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e4right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l4right.pack(pady=10)
     e4right.pack()
-    e5right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e5right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l5right.pack(pady=10)
     e5right.pack()
-    e6right = tk.Label(frameRight, borderwidth=1, relief="solid", width=25)
+    e6right = tk.Label(curWorkspacePoseFrame, borderwidth=1, relief="solid", width=25)
     l6right.pack(pady=10)
     e6right.pack()
 
@@ -272,16 +272,16 @@ def main():
                         )
     eoatEstop.pack(pady=1)
     #end eoat frame
-    masterThread = None
+    mainTkThread = None
 
     rospy.init_node('joint_publisher_gui')
     pub = rospy.Publisher("pointcloudOnOff", Bool, queue_size=10)
     
     global toggleButtonState, eoatPub
-    toggle_btn = tk.Button(frameRight, text="Start Scan", relief="raised",command = lambda: Thread(target=scanButton).start())
+    toggle_btn = tk.Button(curWorkspacePoseFrame, text="Start Scan", relief="raised",command = lambda: Thread(target=scanButton).start())
     toggle_btn.pack(pady=20)
     rate = rospy.Rate(60)
-    masterThread = None
+    mainTkThread = None
     while not rospy.is_shutdown():
         try:
             eoatPub = rospy.Publisher("mmm_eoat_command", Point32)
@@ -289,14 +289,14 @@ def main():
             rospy.Subscriber("mmm_eoat_position", Point32, lambda msg: Thread(eoatCallback(msg,eoatLeftFingerValue,eoatRightFingerValue,eoatStatusReadout,eoatCalabrate,eoatSetOffset,eoatSendPose)).start())
             
             #start window
-            masterThread = Thread(master.mainloop()).start()
+            mainTkThread = Thread(mainTk.mainloop()).start()
             rate.sleep()
         except KeyboardInterrupt:
             print("Recived Keyboard Interrupt =============================================")
-            masterThread.join()
-            master.destroy()
-    masterThread.join()
-    master.destroy()
+            mainTkThread.join()
+            mainTk.destroy()
+    mainTkThread.join()
+    mainTk.destroy()
 
 # function for calabrating the end of arm tooling
 def eoatPublisher(_x, _y, _z):
@@ -353,21 +353,21 @@ def scanButton():
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
 
-def frameToggle(frameleftAlt,frameLeft,jButtoncenter):
+def frameToggle(workSpacePoseFrame,jointSpacePoseFrame,jButtoncenter):
     global altTop
     if altTop:
-        frameLeft.tkraise()
+        jointSpacePoseFrame.tkraise()
         altTop = False
         jButtoncenter["text"] = 'Switch Goal to Work Space'
 
     else:
-        frameleftAlt.tkraise()
+        workSpacePoseFrame.tkraise()
         altTop = True
         jButtoncenter["text"] = 'Switch Goal to Joint Space'
 
-def killPorgram(master):
+def killPorgram(mainTk):
     rospy.signal_shutdown("GUI Closed")
-    master.destroy()
+    mainTk.destroy()
 
 
 
